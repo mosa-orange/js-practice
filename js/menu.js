580b8d7228e16b41fc653874080e471098e87a05
k_miymaoto <canary16@gmail.com>
2020-07-08 12:57:33 +0900

    menu再構築

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
+// jQuery対応
+
+$(function() {
     $("#js-hamburger").click(function () {
         $(this).toggleClass("active");
-        $(this).prev().toggleClass("active");
+        $(this).prevAll().toggleClass("active");
         $('.header__navItem').hide();
         $('.header__navItem').each(function(i) {
-            $(this).delay(80 * i).fadeIn(500);
+            $(this).delay(50 * i).fadeIn(450);
         });
     });
 });
+
+// JS対応
