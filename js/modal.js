7fc9a7f4e2dbe16f07894746ca5a668f833ef0c4
k_miymaoto <canary16@gmail.com>
2020-07-09 14:11:09 +0900

    モーダル追加

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,42 @@
-'use strict';
+// jQuery対応
+$(function(){
+    $("#js-open").click(modalOpen);
 
+    $("#js-close").click(modalClose);
+
+    $("#js-mask").click(modalClose);
+
+    function modalOpen (){
+        $("#js-mask").removeClass("is-hidden");
+        $("#js-modal").removeClass("is-hidden");
+    }
+
+    function modalClose (){
+        $("#js-mask").addClass("is-hidden");
+        $("#js-modal").addClass("is-hidden");
+    }
+
+});
+
+// JS対応
+'use strict';
 {
-    const open = document.getElementById('open');
-    const close = document.getElementById('close');
-    const modal = document.getElementById('modal');
-    const mask = document.getElementById('mask');
+    const open = document.getElementById('js-open');
+    const close = document.getElementById('js-close');
+    const modal = document.getElementById('js-modal');
+    const mask = document.getElementById('js-mask');
 
     open.addEventListener('click', () => {
-        modal.classList.remove('hidden');
-        mask.classList.remove('hidden');
+        modal.classList.remove('is-hidden');
+        mask.classList.remove('is-hidden');
     });
 
     close.addEventListener('click', () => {
-        modal.classList.add('hidden');
-        mask.classList.add('hidden');
+        modal.classList.add('is-hidden');
+        mask.classList.add('is-hidden');
     });
 
     mask.addEventListener('click', () => {
-        // modal.classList.add('hidden');
-        // mask.classList.add('hidden');
         close.click();
     });
 }
