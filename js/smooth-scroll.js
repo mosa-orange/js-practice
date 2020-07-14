b8adfe5a222224812602a49f78c66f6143828ae3
k_miymaoto <canary16@gmail.com>
2020-07-15 02:28:47 +0900

    スムーススクロール実装

---
+++ js/smooth-scroll.js
@@ -0,0 +1,28 @@
+// jQuery対応
+var smoothScrollBtn = $("#smoothScroll");
+
+$(function(){
+    //smoothScrollの姿を消す
+    smoothScrollBtn.hide();
+
+    //ウィンドウがある程度スクロールされたら、#smoothScrollが出現、それ以外のときは非表示
+    $(window).scroll(function(){
+        $(this).scrollTop();
+        //console.log($(this).scrollTop());
+        if($(this).scrollTop() > 850){
+            smoothScrollBtn.fadeIn();
+        }else{
+            smoothScrollBtn.fadeOut();
+        }
+    });
+    //クリックでページトップへもどる
+    smoothScrollBtn.on('click',function(){
+        $('html, body').animate({
+            scrollTop:0
+        },
+        1500
+        );
+    });
+});
+
+// JS対応
