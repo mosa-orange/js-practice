ea4c1c1e8ff8e59077a47f28ff1b912933631146
k_miymaoto <canary16@gmail.com>
2020-07-17 04:49:21 +0900

    ドロップダウンメニュー実装

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,17 @@
 // jQuery対応
+$(function(){
+    var menuCategoryWrap = $('.dropdownMenu__listItemWrap');
+
+    $(function(){
+        menuCategoryWrap
+        .on('mouseover',function(){
+            $(this).children('ul').addClass('is-active');
+        })
+
+        .on('mouseout',function(){
+            $(this).children('ul').removeClass('is-active');
+        });
+    });
+});
 
 // JS対応
