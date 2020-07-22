2f78d13cf33e65012e6e0076bb493e79a696bd2a
k_miymaoto <canary16@gmail.com>
2020-07-22 09:50:43 +0900

    jQueryカルーセル slickで実装

---
+++ js/clusel-slick.js
@@ -0,0 +1,10 @@
+// jQuery対応
+$(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        adaptiveHeight: true,
+    });
+});
+// JS対応
