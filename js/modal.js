c7737ba029be99b0f049907458cd31ba2e2f3c02
k_miymaoto <canary16@gmail.com>
2020-08-03 20:25:41 +0900

    JS：モーダル完了

--- js/modal.js
+++ js/modal.js
@@ -1,43 +1,42 @@
 // jQuery対応
-var hiddenClass = "is-hidden";
-var openBtn = $("#js-open");
-var closeBtn = $("#js-close,#js-mask");
-var modalArea = $("#js-modal");
+// var hiddenClass = "is-hidden";
+// var openBtn = $("#js-open");
+// var closeBtn = $("#js-close,#js-mask");
+// var modalArea = $("#js-modal");
 
-$(function(){
-    openBtn.on('click',modalOpen);
-    closeBtn.on('click',modalClose);
+// $(function(){
+//     openBtn.on('click',modalOpen);
+//     closeBtn.on('click',modalClose);
 
-    function modalOpen(){
-        closeBtn.removeClass(hiddenClass);
-        modalArea.removeClass(hiddenClass);
-    }
+//     function modalOpen(){
+//         closeBtn.removeClass(hiddenClass);
+//         modalArea.removeClass(hiddenClass);
+//     }
 
-    function modalClose(){
-        closeBtn.addClass(hiddenClass);
-        modalArea.addClass(hiddenClass);
-    }
-});
+//     function modalClose(){
+//         closeBtn.addClass(hiddenClass);
+//         modalArea.addClass(hiddenClass);
+//     }
+// });
 
 // JS対応
-'use strict';
 {
-    const open = document.getElementById('js-open');
-    const close = document.getElementById('js-close');
+    const openBtn = document.getElementById('js-open');
+    const closeBtn = document.getElementById('js-close');
     const modal = document.getElementById('js-modal');
     const mask = document.getElementById('js-mask');
 
-    open.addEventListener('click',function(){
+    openBtn.addEventListener('click',function(){
         modal.classList.remove('is-hidden');
         mask.classList.remove('is-hidden');
     });
 
-    close.addEventListener('click',function(){
+    closeBtn.addEventListener('click',function(){
         modal.classList.add('is-hidden');
         mask.classList.add('is-hidden');
     });
 
     mask.addEventListener('click',function(){
-        close.click();
+        closeBtn.click();
     });
 }
